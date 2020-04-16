7fdd8d6ab630a83b0c1953e67033fbbe28fb0080
julienemo <juliette.nada@gmail.com>
2020-04-16 14:43:23 +0200

    [add] show single movie detail on detail btn click

--- index.js
+++ index.js
@@ -2,38 +2,7 @@
 const key = '8de0df23'
 const endPoint = 'http://www.omdbapi.com/?apikey='
 
-// prepare dom targets
-const form = document.querySelector('form');
-const keyWordZone = document.querySelector('#keyword');
-const gallery = document.querySelector('#gallery');
-const content = document.querySelector('#content');
-const btnsDetail = document.querySelectorAll('.btn_detail');
-
-// page filling
-const movieCard = (movie, galleryZone) => {
-  console.log(`${movie.imdbID} ${movie.Poster}`)
-  if (movie.Poster == 'N/A') {
-    movie.Poster = 'images/default.png'
-  }
-  galleryZone.innerHTML += `
-  <div class='col col-4 mb-4'>
-    <img class="poster" src="${movie.Poster}" alt="${movie.Title} poster">
-    <p>${movie.Title} (${movie.Year})</p>
-    <button id="${movie.imdbId}" class='btn_detail btn btn-info'>See detail</button>
-  </div>
-  `
-}
-
-const showResults = () => {
-  if (keyWordZone.value.length < 3) {
-    gallery.innerHTML = "Please provide a longer keyword."
-    return
-  } else {
-    apiCall(keyWordZone);
-  }
-}
-
-const apiCall = (keyWordZone) => {
+const moviesCall = (keyWordZone) => {
   fetch(`${endPoint}${key}&s=${keyWordZone.value}`)
   .then((response) => (response.json()))
   .then((response) => {
@@ -44,16 +13,62 @@ const apiCall = (keyWordZone) => {
     })
   }).catch((error) => {
     console.log(error);
-    gallery.innerHTML = "An error occured. Please contact service provider or retry later"
+    gallery.innerHTML = "An error ocurred. Please contact service provider or retry later"
   })
-
 }
 
-const showMovieDetail = () => {
- 
+// prepare dom targets
+const form = document.querySelector('form');
+const keyWordZone = document.querySelector('#keyword');
+const gallery = document.querySelector('#gallery');
+
+const modal = document.querySelector('.modal');
+const btnClose = document.querySelector('#btn_close');
+const detailTitle = document.querySelector('#detail_title');
+const detailPlot = document.querySelector('#detail_plot');
+
+// page filling
+const movieCard = (movie, galleryZone) => {
+  if (movie.Poster == 'N/A') {
+    movie.Poster = 'images/default.png'
+  }
+  galleryZone.innerHTML += `
+  <div class='col col-4 mb-4'>
+    <img class="poster" src="${movie.Poster}" alt="${movie.Title} poster">
+    <p>${movie.Title} (${movie.Year})</p>
+    <button id="${movie.imdbID}" data-toggle="modal" onclick="showMovieDetail('${movie.imdbID}')"  class='btn_detail btn btn-info'>See detail</button>
+  </div>
+  `
 }
+
+const showResults = (e) => {
+  e.preventDefault();
+  if (keyWordZone.value.length < 3) {
+    gallery.innerHTML = "Please provide a longer keyword."
+    return
+  } else {
+    moviesCall(keyWordZone);
+  }
+}
+
+// show single movie detail
+const showMovieDetail = (movieId) => {
+  fetch(`${endPoint}${key}&i=${movieId}`)
+  .then((response) => (response.json()))
+  .then((response) => {
+    modal.style.display = 'block';
+    detailTitle.innerHTML = `${response.Title} (${response.Released})`;
+    detailPlot.innerHTML = `${response.Plot}`; 
+  }).catch((error) => {
+    modal.style.display = 'block';
+    detailTitle.innerHTML = `Error`;
+    detailPlot.innerHTML = `Movie detail cannot be retrieved. Please retry or contact service provider`; 
+  })
+}
+
 // page load, event attribution
 form.onsubmit = showResults;
-btnsDetail.forEach((btn) => {
-  //btn.onclick = 
-});
+
+btnClose.onclick = () => {
+  modal.style.display = 'none';
+}
